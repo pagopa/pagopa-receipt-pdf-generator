38a01150005108d14bd9c7db43fae591eef881c0
svariant <samuele.varianti@nttdata.com>
2023-09-12 11:14:11 +0200

    [PRDP-130] Fixed http request to queue

--- performance-test/src/modules/queue_client.js
+++ performance-test/src/modules/queue_client.js
@@ -1,44 +1,11 @@
 import http from 'k6/http';
-import crypto from 'k6/crypto';
 
-export function sendMessageToQueue(message, url, accountName, accountKey, queueName){
-    let headers = getAPIHeaders(accountName, accountKey, "POST", queueName);
+export function sendMessageToQueue(message, url, accountKey){
+    let headers = {
+        'x-ms-date': new Date().toUTCString()
+    };
 
     let body = `<QueueMessage><MessageText>${message}</MessageText></QueueMessage>`;
     
-    return http.post(url, body, { headers });
-}
-
-function getAPIHeaders(accountName, accountKey, httpMethod, queueName){
-
-    return {
-        'Authorization': generateAPISharedKey(accountName, accountKey, httpMethod, queueName),
-        'x-ms-date': new Date().toUTCString()
-    };
-}
-
-function generateAPISharedKey(accountName, accountKey, httpMethod, queueName){
-    const resourcePath = `/${queueName}`;
-    
-    // Crea la stringa da firmare
-    const date = new Date().toUTCString();
-    const stringToSign = httpMethod + "\n" +  
-     "\n" +  
-    "\n" +  
-     "\n" +  
-     "\n" +  
-     "\n" +  
-     "\n" +  
-     "\n" +  
-     "\n" +  
-     "\n" +  
-     "\n" +  
-    `nx-ms-date:${date}` + "\n" +
-    resourcePath;
-    
-    // Calcola la firma HMAC-SHA256
-    const signature = crypto.hmac("sha256", accountKey, encodeURI(stringToSign), "hex");
-    
-    // Crea l'intestazione "Authorization"
-    return `SharedKey ${accountName}:${signature}`;
+    return http.post(`${url}${accountKey}`, body, { headers });
 }
