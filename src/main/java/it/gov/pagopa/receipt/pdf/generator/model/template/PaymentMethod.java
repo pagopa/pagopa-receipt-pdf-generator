a7bf04d50679e7704d491134d7f418563c621429
giomella <gioele.mella.st@everis.nttdata.com>
2023-09-25 11:24:34 +0200

    [PRDP-70] added pdf template models

---
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/model/template/PaymentMethod.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.receipt.pdf.generator.model.template;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.*;
+
+@Data
+@Builder
+@JsonInclude(Include.NON_NULL)
+public class PaymentMethod {
+
+    private String name;
+    private String logo;
+    private String accountHolder;
+    private boolean extraFee;
+
+}
---
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/model/template/Transaction.java
@@ -0,0 +1,22 @@
+package it.gov.pagopa.receipt.pdf.generator.model.template;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import static com.fasterxml.jackson.annotation.JsonInclude.*;
+
+@Data
+@Builder
+@JsonInclude(Include.NON_NULL)
+public class Transaction {
+
+    private String id;
+    private String timestamp;
+    private double amount;
+    private PSP psp;
+    private String rrn;
+    private String authCode;
+    private PaymentMethod paymentMethod;
+
+}
