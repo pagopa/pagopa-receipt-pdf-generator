9f9be882a12818d40b1a6b70489fbd29e28480ab
acialini <alessio.cialini@emeal.nttdata.com>
2023-09-28 16:37:35 +0200

    [PRDP-104] feat: introducing cart data for notification in receipt entity

--- src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/receipt/EventData.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/receipt/EventData.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 @NoArgsConstructor
@@ -11,4 +13,5 @@ public class EventData {
     private String payerFiscalCode;
     private String debtorFiscalCode;
     private String transactionCreationDate;
+    private List<CartItem> cart;
 }
