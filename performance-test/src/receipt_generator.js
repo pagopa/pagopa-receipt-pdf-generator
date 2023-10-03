ab293256d62485222f3f7ce0ca7d638ff6e0e63c
svariant <samuele.varianti@nttdata.com>
2023-10-03 12:37:03 +0200

    [PRDP-130] Log

--- performance-test/src/receipt_generator.js
+++ performance-test/src/receipt_generator.js
@@ -99,7 +99,7 @@ export default function () {
 		let responseQueue = sendMessageToQueue(eventMessage, receiptQueueURI, receiptQueuePrimaryKey);
 
 		console.log("SendMessageToQueue call, Status " + responseQueue.status);
-		console.log("RESPONSE QUEUE" + responseQueue.body);
+		console.log("RESPONSE QUEUE" + receiptQueueURI + " " + receiptQueuePrimaryKey +  " " + responseQueue.error + " " + responseQueue.errorMessage);
 	
 		check(eventMessage, {
 			'SendMessageToQueue status is 200': (_response) => responseQueue.status === 200,
