db640bcb37dcfa01977e17e88054fa373b4192dc
svariant <samuele.varianti@nttdata.com>
2023-10-09 12:57:01 +0200

    [PRDP-155] Added JsonIgnoreProperties ignoreUnknown to BizEvent's entities

--- src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/event/AuthRequest.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/event/AuthRequest.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.receipt.pdf.generator.entity.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
@@ -8,6 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class AuthRequest {
 	private String authOutcome;
 	private String guid;
--- src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/event/TransactionDetails.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/event/TransactionDetails.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.receipt.pdf.generator.entity.event;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Getter
@@ -7,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TransactionDetails {
 	private String origin;
 	private User user;
