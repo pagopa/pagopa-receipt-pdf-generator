fc3e9ec7afd16d2e0b9a61e35fd546bb5c7ec8fb
giomella <gioele.mella.st@everis.nttdata.com>
2023-10-11 17:44:25 +0200

    [PRDP-161] refactor temp file handling

--- src/main/java/it/gov/pagopa/receipt/pdf/generator/client/PdfEngineClient.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/client/PdfEngineClient.java
@@ -3,7 +3,9 @@ package it.gov.pagopa.receipt.pdf.generator.client;
 import it.gov.pagopa.receipt.pdf.generator.model.request.PdfEngineRequest;
 import it.gov.pagopa.receipt.pdf.generator.model.response.PdfEngineResponse;
 
+import java.nio.file.Path;
+
 public interface PdfEngineClient {
 
-    PdfEngineResponse generatePDF(PdfEngineRequest pdfEngineRequest);
+    PdfEngineResponse generatePDF(PdfEngineRequest pdfEngineRequest, Path workingDirPath);
 }
