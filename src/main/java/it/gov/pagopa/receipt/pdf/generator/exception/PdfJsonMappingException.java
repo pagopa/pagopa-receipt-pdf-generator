3abb9c315f1a80010e65b6b9fcbb49cb05155da7
acialini <alessio.cialini@emeal.nttdata.com>
2023-10-17 10:47:43 +0200

    [PRDP-145] feat: update usage to recover from json (to complete)

--- src/main/java/it/gov/pagopa/receipt/pdf/generator/exception/PdfJsonMappingException.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/exception/PdfJsonMappingException.java
@@ -3,6 +3,6 @@ package it.gov.pagopa.receipt.pdf.generator.exception;
 import com.fasterxml.jackson.core.JsonProcessingException;
 
 public class PdfJsonMappingException extends RuntimeException {
-    public PdfJsonMappingException(JsonProcessingException e) {
+    public PdfJsonMappingException(Exception e) {
     }
 }
