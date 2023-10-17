f4126ed4a6a3c2ae76ccbe9d4e7a524e545701cd
svariant <samuele.varianti@nttdata.com>
2023-10-17 16:51:03 +0200

    [PRDP-169] Added exception to template data mapping

--- src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/receipt/enumeration/ReasonErrorCode.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/entity/receipt/enumeration/ReasonErrorCode.java
@@ -1,7 +1,7 @@
 package it.gov.pagopa.receipt.pdf.generator.entity.receipt.enumeration;
 
 public enum ReasonErrorCode {
-    ERROR_QUEUE(902), ERROR_BLOB_STORAGE(901), ERROR_PDF_ENGINE(0);
+    ERROR_QUEUE(902), ERROR_BLOB_STORAGE(901), ERROR_PDF_ENGINE(0), ERROR_TEMPLATE_PDF(903);
 
     private int code;
 
--- src/main/java/it/gov/pagopa/receipt/pdf/generator/utils/BizEventToPdfMapper.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/utils/BizEventToPdfMapper.java
@@ -2,7 +2,9 @@ package it.gov.pagopa.receipt.pdf.generator.utils;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import it.gov.pagopa.receipt.pdf.generator.entity.event.BizEvent;
+import it.gov.pagopa.receipt.pdf.generator.entity.receipt.enumeration.ReasonErrorCode;
 import it.gov.pagopa.receipt.pdf.generator.exception.PdfJsonMappingException;
+import it.gov.pagopa.receipt.pdf.generator.exception.TemplateDataMappingException;
 import it.gov.pagopa.receipt.pdf.generator.model.template.PSP;
 import it.gov.pagopa.receipt.pdf.generator.model.template.PSPFee;
 
@@ -52,189 +54,236 @@ public class BizEventToPdfMapper {
     private BizEventToPdfMapper() {
     }
 
-    public static String getId(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getTransaction() != null &&
-                event.getTransactionDetails().getTransaction().getIdTransaction() != 0L) {
+    public static String getId(BizEvent event) throws TemplateDataMappingException {
+        if (
+                event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getTransaction() != null &&
+                        event.getTransactionDetails().getTransaction().getIdTransaction() != 0L
+        ) {
             return String.valueOf(event.getTransactionDetails().getTransaction().getIdTransaction());
         }
         if (event.getPaymentInfo() != null) {
-            return event.getPaymentInfo().getPaymentToken() != null ? event.getPaymentInfo().getPaymentToken()
-                    : event.getPaymentInfo().getIUR();
+            if (event.getPaymentInfo().getPaymentToken() != null) {
+                return event.getPaymentInfo().getPaymentToken();
+            }
+            if (event.getPaymentInfo().getIUR() != null) {
+                return event.getPaymentInfo().getIUR();
+            }
         }
-
-        return null;
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.id"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getTimestamp(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getTransaction() != null &&
-                event.getTransactionDetails().getTransaction().getCreationDate() != null) {
+    public static String getTimestamp(BizEvent event) throws TemplateDataMappingException {
+        if (
+                event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getTransaction() != null &&
+                        event.getTransactionDetails().getTransaction().getCreationDate() != null
+        ) {
             return dateFormat(event.getTransactionDetails().getTransaction().getCreationDate(), true);
         }
-
-        return event.getPaymentInfo() != null ? dateFormat(event.getPaymentInfo().getPaymentDateTime(), false) : null;
+        if (event.getPaymentInfo() != null && event.getPaymentInfo().getPaymentDateTime() != null) {
+            return dateFormat(event.getPaymentInfo().getPaymentDateTime(), false);
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.timestamp"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getAmount(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getTransaction() != null &&
-                event.getTransactionDetails().getTransaction().getAmount() != 0L) {
+    public static String getAmount(BizEvent event) throws TemplateDataMappingException {
+        if (
+                event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getTransaction() != null &&
+                        event.getTransactionDetails().getTransaction().getGrandTotal() != 0L
+        ) {
             // Amount in transactionDetails is defined in cents (es. 25500 not 255.00)
             return currencyFormat(String.valueOf(event.getTransactionDetails().getTransaction().getGrandTotal() / 100.00));
         }
-
-        return event.getPaymentInfo() != null ? currencyFormat(event.getPaymentInfo().getAmount()) : null;
+        if (event.getPaymentInfo() != null && event.getPaymentInfo().getAmount() != null) {
+            return currencyFormat(event.getPaymentInfo().getAmount());
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.amount"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getPspFee(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getTransaction() != null &&
-                event.getTransactionDetails().getTransaction().getFee() != 0L) {
+    public static String getPspFee(BizEvent event) throws TemplateDataMappingException {
+        if (
+                event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getTransaction() != null &&
+                        event.getTransactionDetails().getTransaction().getFee() != 0L
+        ) {
             // Fee in transactionDetails is defined in cents (es. 25500 not 255.00)
             return currencyFormat(String.valueOf(event.getTransactionDetails().getTransaction().getFee() / 100.00));
         }
-
-        return event.getPaymentInfo() != null ? currencyFormat(event.getPaymentInfo().getFee()) : null;
+        if (event.getPaymentInfo() != null &&
+                event.getPaymentInfo().getFee() != null) {
+            return currencyFormat(event.getPaymentInfo().getFee());
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.psp.fee"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getRnn(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getTransaction() != null &&
-                event.getTransactionDetails().getTransaction().getRrn() != null) {
+    public static String getRnn(BizEvent event) throws TemplateDataMappingException {
+        if (
+                event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getTransaction() != null &&
+                        event.getTransactionDetails().getTransaction().getRrn() != null
+        ) {
             return event.getTransactionDetails().getTransaction().getRrn();
         }
-
         if (event.getPaymentInfo() != null) {
-            return event.getPaymentInfo().getPaymentToken() != null ? event.getPaymentInfo().getPaymentToken()
-                    : event.getPaymentInfo().getIUR();
+            if (event.getPaymentInfo().getPaymentToken() != null) {
+                return event.getPaymentInfo().getPaymentToken();
+            }
+            if (event.getPaymentInfo().getIUR() != null) {
+                return event.getPaymentInfo().getIUR();
+            }
         }
 
-        return null;
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.rrn"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
     public static String getAuthCode(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getTransaction() != null) {
+        if (event.getTransactionDetails() != null && event.getTransactionDetails().getTransaction() != null) {
             return event.getTransactionDetails().getTransaction().getAuthorizationCode();
         }
 
         return null;
     }
 
-    public static String getPaymentMethodName(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getWallet() != null &&
-                event.getTransactionDetails().getWallet().getInfo() != null) {
+    public static String getPaymentMethodName(BizEvent event) throws TemplateDataMappingException {
+        if (
+                event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getWallet() != null &&
+                        event.getTransactionDetails().getWallet().getInfo() != null &&
+                        event.getTransactionDetails().getWallet().getInfo().getBrand() != null
+        ) {
             return event.getTransactionDetails().getWallet().getInfo().getBrand();
         }
 
-        return null;
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.paymentMethod.name"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getPaymentMethodLogo(BizEvent event) {
-        // TODO analyse -> transactionDetails.wallet.info.brandLogo doesn't exist
-
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getWallet() != null &&
-                event.getTransactionDetails().getWallet().getInfo() != null) {
-            return brandLogoMap.getOrDefault(
-                    event.getTransactionDetails().getWallet()
-                            .getInfo().getBrand(),
-                    null);
-        }
-
-        return null;
+    public static String getPaymentMethodLogo(BizEvent event) throws TemplateDataMappingException {
+        return brandLogoMap.getOrDefault(getPaymentMethodName(event), null);
     }
 
     public static String getPaymentMethodAccountHolder(BizEvent event) {
-        if (event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getWallet() != null &&
-                event.getTransactionDetails().getWallet().getInfo() != null &&
-                event.getTransactionDetails().getWallet().getInfo().getHolder() != null) {
+        if (
+                event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getWallet() != null &&
+                        event.getTransactionDetails().getWallet().getInfo() != null &&
+                        event.getTransactionDetails().getWallet().getInfo().getHolder() != null
+        ) {
             return event.getTransactionDetails().getWallet().getInfo().getHolder();
         }
-
         return event.getPayer() != null ? event.getPayer().getFullName() : null;
     }
 
-    public static String getUserMail() {
-        return null;
-    }
-
-    public static String getUserFullName(BizEvent event) {
-        return event.getPayer() != null ? event.getPayer().getFullName() : null;
-    }
-
-    public static String getUserTaxCode(BizEvent event) {
-        return event.getPayer() != null ? event.getPayer().getEntityUniqueIdentifierValue() : null;
-    }
-
-    public static String getRefNumberType(BizEvent event) {
-        if (event.getDebtorPosition() != null &&
-                event.getDebtorPosition().getModelType() != null &&
-                event.getDebtorPosition().getModelType().equals("2")) {
-            return REF_TYPE_IUV;
+    public static String getUserFullName(BizEvent event) throws TemplateDataMappingException {
+        if (event.getPayer() != null && event.getPayer().getFullName() != null) {
+            return event.getPayer().getFullName();
         }
-
-        return REF_TYPE_NOTICE;
+        throw new TemplateDataMappingException(formatErrorMessage("user.data.fullName"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getRefNumberValue(BizEvent event) {
-        return event.getDebtorPosition() != null ? event.getDebtorPosition().getIuv() : null;
+    public static String getUserTaxCode(BizEvent event) throws TemplateDataMappingException {
+        if (event.getPayer() != null && event.getPayer().getEntityUniqueIdentifierValue() != null) {
+            return event.getPayer().getEntityUniqueIdentifierValue();
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("user.data.taxCode"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
+    }
+
+    public static String getRefNumberType(BizEvent event) throws TemplateDataMappingException {
+        if (event.getDebtorPosition() != null && event.getDebtorPosition().getModelType() != null) {
+            if (event.getDebtorPosition().getModelType().equals("1")) {
+                return REF_TYPE_NOTICE;
+            }
+            if (event.getDebtorPosition().getModelType().equals("2")) {
+                return REF_TYPE_IUV;
+            }
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("cart.item.refNumber.type"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
+    }
+
+    public static String getRefNumberValue(BizEvent event) throws TemplateDataMappingException {
+        if (event.getDebtorPosition() != null && event.getDebtorPosition().getIuv() != null) {
+            return event.getDebtorPosition().getIuv();
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("cart.item.refNumber.value"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
     public static String getDebtorFullName(BizEvent event) {
         return event.getDebtor() != null ? event.getDebtor().getFullName() : null;
     }
 
-    public static String getDebtorTaxCode(BizEvent event) {
-        return event.getDebtor() != null ? event.getDebtor().getEntityUniqueIdentifierValue() : null;
+    public static String getDebtorTaxCode(BizEvent event) throws TemplateDataMappingException {
+        if (event.getDebtor() != null && event.getDebtor().getEntityUniqueIdentifierValue() != null) {
+            return event.getDebtor().getEntityUniqueIdentifierValue();
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("cart.item.debtor.taxCode"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
     public static String getPayeeName(BizEvent event) {
         return event.getCreditor() != null ? event.getCreditor().getOfficeName() : null;
     }
 
-    public static String getPayeeTaxCode(BizEvent event) {
-        return event.getCreditor() != null ? event.getCreditor().getCompanyName() : null;
+    public static String getPayeeTaxCode(BizEvent event) throws TemplateDataMappingException {
+        if (event.getCreditor() != null && event.getCreditor().getCompanyName() != null) {
+            return event.getCreditor().getCompanyName();
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("cart.item.payee.taxCode"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getItemSubject(BizEvent event) {
-        return event.getPaymentInfo() != null ? event.getPaymentInfo().getRemittanceInformation() : null;
+    public static String getItemSubject(BizEvent event) throws TemplateDataMappingException {
+        if (event.getPaymentInfo() != null && event.getPaymentInfo().getRemittanceInformation() != null) {
+            return event.getPaymentInfo().getRemittanceInformation();
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("cart.item.subject"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static String getItemAmount(BizEvent event) {
-        return event.getPaymentInfo() != null ? currencyFormat(event.getPaymentInfo().getAmount()) : null;
+    public static String getItemAmount(BizEvent event) throws TemplateDataMappingException {
+        if (event.getPaymentInfo() != null && event.getPaymentInfo().getAmount() != null) {
+            return currencyFormat(event.getPaymentInfo().getAmount());
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("cart.item.amount"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
     }
 
-    public static PSP getPsp(BizEvent event) {
+    private static String getPspName(BizEvent event) throws TemplateDataMappingException {
+        if (event.getTransactionDetails().getTransaction().getPsp().getBusinessName() != null) {
+            return event.getTransactionDetails().getTransaction().getPsp().getBusinessName();
+        }
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.psp.name"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
+    }
 
-        if (event.getPsp() != null &&
-                event.getPsp().getIdPsp() != null &&
-                event.getTransactionDetails() != null &&
-                event.getTransactionDetails().getTransaction() != null &&
-                event.getTransactionDetails().getTransaction().getPsp() != null) {
-            String name = event.getTransactionDetails().getTransaction().getPsp().getBusinessName();
+    public static PSP getPsp(BizEvent event) throws TemplateDataMappingException {
+        if (
+                event.getPsp() != null &&
+                        event.getPsp().getIdPsp() != null &&
+                        event.getTransactionDetails() != null &&
+                        event.getTransactionDetails().getTransaction() != null &&
+                        event.getTransactionDetails().getTransaction().getPsp() != null
+        ) {
             LinkedHashMap<String, String> info = (LinkedHashMap<String, String>) pspMap
                     .getOrDefault(event.getPsp().getIdPsp(), new LinkedHashMap<>());
             return PSP.builder()
-                    .name(name)
+                    .name(getPspName(event))
                     .fee(PSPFee.builder()
                             .amount(getPspFee(event))
                             .build())
-                    .companyName(info.get("companyName"))
-                    .address(info.get("address"))
-                    .city(info.get("city"))
-                    .province(info.get("province"))
-                    .buildingNumber(info.get("buildingNumber"))
-                    .postalCode(info.get("postalCode"))
-                    .logo(info.get("logo"))
+                    .companyName(getOrThrow(info, "companyName"))
+                    .address(getOrThrow(info, "address"))
+                    .city(getOrThrow(info, "city"))
+                    .province(getOrThrow(info, "province"))
+                    .buildingNumber(getOrThrow(info, "buildingNumber"))
+                    .postalCode(getOrThrow(info, "postalCode"))
+                    .logo(getOrThrow(info, "logo"))
                     .build();
-
         }
+        throw new TemplateDataMappingException(formatErrorMessage("transaction.psp"), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
+    }
 
-        return null;
+    private static String getOrThrow(LinkedHashMap<String, String> map, String key) throws TemplateDataMappingException {
+        String value = map.get(key);
+        if (value == null) {
+            throw new TemplateDataMappingException(formatErrorMessage(String.format("transaction.psp.%s", key)), ReasonErrorCode.ERROR_TEMPLATE_PDF.getCode());
+        }
+        return value;
     }
 
     private static String currencyFormat(String value) {
@@ -242,18 +291,20 @@ public class BizEventToPdfMapper {
         NumberFormat numberFormat = NumberFormat.getInstance(Locale.ITALY);
         numberFormat.setMaximumFractionDigits(2);
         numberFormat.setMinimumFractionDigits(2);
-
         return numberFormat.format(valueToFormat);
     }
 
-    private static String dateFormat(String date, boolean withTimeZone){
+    private static String dateFormat(String date, boolean withTimeZone) {
         DateTimeFormatter simpleDateFormat = DateTimeFormatter.ofPattern("dd MMMM yyyy, HH:mm:ss").withLocale(Locale.ITALY);
-
         if (withTimeZone) {
             return ZonedDateTime.parse(date).format(simpleDateFormat);
         }
-
         return LocalDateTime.parse(date).format(simpleDateFormat);
+    }
+
+    private static String formatErrorMessage(String missingProperty) {
+        return String.format("Error mapping bizEvent data to template, missing property %s",
+                missingProperty);
     }
 
 }
