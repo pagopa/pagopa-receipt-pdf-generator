5bcd2da961036a223ca18e394ff1d78638625531
svariant <samuele.varianti@nttdata.com>
2023-12-01 16:21:17 +0100

    [PRDP-262] Implemented method to save Receipt document to cosmos and custom exception

--- src/main/java/it/gov/pagopa/receipt/pdf/generator/client/ReceiptCosmosClient.java
+++ src/main/java/it/gov/pagopa/receipt/pdf/generator/client/ReceiptCosmosClient.java
@@ -1,9 +1,25 @@
 package it.gov.pagopa.receipt.pdf.generator.client;
 
+import com.azure.cosmos.models.CosmosItemResponse;
 import it.gov.pagopa.receipt.pdf.generator.entity.receipt.Receipt;
 import it.gov.pagopa.receipt.pdf.generator.exception.ReceiptNotFoundException;
 
 public interface ReceiptCosmosClient {
 
-    Receipt getReceiptDocument(String receiptId) throws ReceiptNotFoundException;
+    /**
+     * Retrieve receipt document from CosmosDB database
+     *
+     * @param eventId Biz-event id
+     * @return receipt document
+     * @throws ReceiptNotFoundException in case no receipt has been found with the given idEvent
+     */
+    Receipt getReceiptDocument(String eventId) throws ReceiptNotFoundException;
+
+    /**
+     * Save Receipts on CosmosDB database
+     *
+     * @param receipt Receipts to save
+     * @return receipt documents
+     */
+    CosmosItemResponse<Receipt> saveReceipt(Receipt receipt);
 }
