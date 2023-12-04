770457bd33773583e073fcfe011b85b7d9e6da57
svariant <samuele.varianti@nttdata.com>
2023-12-04 14:25:53 +0100

    [PRDP-260] Updated unit tests

--- src/test/java/it/gov/pagopa/receipt/pdf/generator/service/impl/BuildTemplateServiceImplTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/generator/service/impl/BuildTemplateServiceImplTest.java
@@ -73,6 +73,7 @@ class BuildTemplateServiceImplTest {
     private static  final String USER_SURNAME = "user_surname";
     private static final String USER_FORMATTED_FULL_NAME = "user_name user_surname";
     private static final String USER_TAX_CODE = "user tax code";
+    public static final String PAGOPA_PA_CHANNEL_ID = "pagopa channel";
     private BuildTemplateServiceImpl buildTemplateService;
 
     @BeforeEach
@@ -130,6 +131,7 @@ class BuildTemplateServiceImplTest {
                                 .psp(TransactionPsp.builder()
                                         .businessName(PSP_NAME)
                                         .build())
+                                .origin(PAGOPA_PA_CHANNEL_ID)
                                 .build())
                         .build())
                 .eventStatus(BizEventStatusType.DONE)
@@ -179,7 +181,7 @@ class BuildTemplateServiceImplTest {
     }
 
     @Test
-    void mapTemplateAllFieldsSuccessCompleteTemplateAndIOPAYChannel() {
+    void mapTemplateAllFieldsSuccessCompleteTemplateAndPagoPaChannelOnTransactionOrigin() {
         BizEvent event = BizEvent.builder()
                 .id(BIZ_EVENT_ID)
                 .idPaymentManager(BIZ_EVENT_ID)
@@ -213,6 +215,102 @@ class BuildTemplateServiceImplTest {
                                 .info(Info.builder().brand(BRAND).holder(HOLDER_FULL_NAME).build())
                                 .onboardingChannel(PAGO_PA_CHANNEL_IO_PAY)
                                 .build())
+                        .transaction(Transaction.builder()
+                                .idTransaction(ID_TRANSACTION)
+                                .grandTotal(GRAND_TOTAL_LONG)
+                                .amount(AMOUNT_LONG)
+                                .fee(FEE_LONG)
+                                .rrn(RRN)
+                                .numAut(AUTH_CODE)
+                                .creationDate(DATE_TIME_TIMESTAMP_ZONED)
+                                .psp(TransactionPsp.builder()
+                                        .businessName(PSP_NAME)
+                                        .build())
+                                .origin(PAGOPA_PA_CHANNEL_ID)
+                                .build())
+                        .build())
+                .eventStatus(BizEventStatusType.DONE)
+                .build();
+        Receipt receipt = Receipt.builder().eventData(EventData.builder().cart(List.of(CartItem.builder().subject(REMITTANCE_INFORMATION).build())).build()).build();
+
+        AtomicReference<ReceiptPDFTemplate> atomicReference = new AtomicReference<>();
+        assertDoesNotThrow(() -> atomicReference.set(buildTemplateService.buildTemplate(event, GENERATED_BY_DEBTOR, receipt)));
+
+        ReceiptPDFTemplate receiptPdfTemplate = atomicReference.get();
+
+        assertNotNull(receiptPdfTemplate);
+        assertEquals(BIZ_EVENT_ID, receiptPdfTemplate.getServiceCustomerId());
+
+        it.gov.pagopa.receipt.pdf.generator.model.template.Transaction transaction = receiptPdfTemplate.getTransaction();
+        assertEquals(DATE_TIME_TIMESTAMP_FORMATTED, transaction.getTimestamp());
+        assertEquals(FORMATTED_GRAND_TOTAL, transaction.getAmount());
+        assertEquals(PSP_LOGO, transaction.getPsp().getLogo());
+        assertEquals(FORMATTED_FEE, transaction.getPsp().getFee().getAmount());
+        assertEquals(PSP_NAME, transaction.getPsp().getName());
+        assertEquals(PSP_CITY, transaction.getPsp().getCity());
+        assertEquals(PSP_COMPANY, transaction.getPsp().getCompanyName());
+        assertEquals(PSP_POSTAL_CODE, transaction.getPsp().getPostalCode());
+        assertEquals(PSP_ADDRESS, transaction.getPsp().getAddress());
+        assertEquals(PSP_BUILDING_NUMBER, transaction.getPsp().getBuildingNumber());
+        assertEquals(PSP_PROVINCE, transaction.getPsp().getProvince());
+        assertEquals(RRN, transaction.getRrn());
+        assertEquals(BRAND, transaction.getPaymentMethod().getName());
+        assertEquals(BRAND_ASSET_URL, transaction.getPaymentMethod().getLogo());
+        assertEquals(HOLDER_FULL_NAME, transaction.getPaymentMethod().getAccountHolder());
+        assertEquals(AUTH_CODE, transaction.getAuthCode());
+        assertEquals(GENERATED_BY_DEBTOR, transaction.isRequestedByDebtor());
+        assertTrue(transaction.isProcessedByPagoPA());
+
+        assertNull(receiptPdfTemplate.getUser());
+
+        it.gov.pagopa.receipt.pdf.generator.model.template.Cart cart = receiptPdfTemplate.getCart();
+        assertEquals(FORMATTED_AMOUNT, cart.getAmountPartial());
+        assertEquals(FORMATTED_AMOUNT, cart.getItems().get(0).getAmount());
+        assertEquals(DEBTOR_FULL_NAME, cart.getItems().get(0).getDebtor().getFullName());
+        assertEquals(DEBTOR_VALID_CF, cart.getItems().get(0).getDebtor().getTaxCode());
+        assertEquals(REMITTANCE_INFORMATION, cart.getItems().get(0).getSubject());
+        assertEquals(COMPANY_NAME, cart.getItems().get(0).getPayee().getName());
+        assertEquals(ID_PA, cart.getItems().get(0).getPayee().getTaxCode());
+        assertEquals(MODEL_TYPE_IUV_TEXT, cart.getItems().get(0).getRefNumber().getType());
+        assertEquals(IUV, cart.getItems().get(0).getRefNumber().getValue());
+    }
+
+    @Test
+    void mapTemplateAllFieldsSuccessCompleteTemplateAndPagoPaChannelOnChannelId() {
+        BizEvent event = BizEvent.builder()
+                .id(BIZ_EVENT_ID)
+                .idPaymentManager(BIZ_EVENT_ID)
+                .debtorPosition(DebtorPosition.builder()
+                        .iuv(IUV)
+                        .modelType(MODEL_TYPE_IUV_CODE)
+                        .build())
+                .creditor(Creditor.builder()
+                        .companyName(COMPANY_NAME)
+                        .idPA(ID_PA)
+                        .build())
+                .psp(Psp.builder()
+                        .idPsp(ID_PSP)
+                        .psp(PSP_NAME)
+                        .build())
+                .debtor(Debtor.builder()
+                        .fullName(DEBTOR_FULL_NAME)
+                        .entityUniqueIdentifierValue(DEBTOR_VALID_CF)
+                        .build())
+                .payer(Payer.builder().fullName(PAYER_FULL_NAME).entityUniqueIdentifierValue(PAYER_VALID_CF).build())
+                .paymentInfo(PaymentInfo.builder()
+                        .paymentDateTime(DATE_TIME_TIMESTAMP_MILLISECONDS)
+                        .paymentToken(PAYMENT_TOKEN)
+                        .amount(AMOUNT_WITHOUT_CENTS)
+                        .fee(FEE_WITH_SINGLE_DIGIT_CENTS)
+                        .remittanceInformation(REMITTANCE_INFORMATION)
+                        .IUR(IUR)
+                        .build())
+                .transactionDetails(TransactionDetails.builder()
+                        .wallet(WalletItem.builder()
+                                .info(Info.builder().brand(BRAND).holder(HOLDER_FULL_NAME).build())
+                                .onboardingChannel(PAGO_PA_CHANNEL_IO_PAY)
+                                .build())
+                        .info(InfoTransaction.builder().clientId(PAGOPA_PA_CHANNEL_ID).build())
                         .transaction(Transaction.builder()
                                 .idTransaction(ID_TRANSACTION)
                                 .grandTotal(GRAND_TOTAL_LONG)
@@ -542,6 +640,7 @@ class BuildTemplateServiceImplTest {
                                 .psp(TransactionPsp.builder()
                                         .businessName(PSP_NAME)
                                         .build())
+                                .origin(PAGOPA_PA_CHANNEL_ID)
                                 .build())
                         .build())
                 .eventStatus(BizEventStatusType.DONE)
@@ -636,6 +735,7 @@ class BuildTemplateServiceImplTest {
                                 .psp(TransactionPsp.builder()
                                         .businessName(PSP_NAME)
                                         .build())
+                                .origin(PAGOPA_PA_CHANNEL_ID)
                                 .build())
                         .build())
                 .eventStatus(BizEventStatusType.DONE)
@@ -730,6 +830,7 @@ class BuildTemplateServiceImplTest {
                                 .psp(TransactionPsp.builder()
                                         .businessName(PSP_NAME)
                                         .build())
+                                .origin(PAGOPA_PA_CHANNEL_ID)
                                 .build())
                         .build())
                 .eventStatus(BizEventStatusType.DONE)
