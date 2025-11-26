4aaa9b906a853a8b488100072b04f8b58b1bef6c
giomella <giomella@emeal.nttdata.com>
2025-11-26 17:08:25 +0100

    improve check on blob storage

--- integration-test/src/step_definitions/receipts_blob_storage_client.js
+++ integration-test/src/step_definitions/receipts_blob_storage_client.js
@@ -1,19 +1,14 @@
 const { BlobServiceClient } = require("@azure/storage-blob");
 
-const blob_storage_conn_string  = process.env.RECEIPTS_STORAGE_CONN_STRING || "";
-const containerName             = process.env.BLOB_STORAGE_CONTAINER_NAME;
+const blob_storage_conn_string = process.env.RECEIPTS_STORAGE_CONN_STRING || "";
+const containerName = process.env.BLOB_STORAGE_CONTAINER_NAME;
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(blob_storage_conn_string);
 const receiptContainerClient = blobServiceClient.getContainerClient(containerName);
 
 async function receiptPDFExist(blobName) {
-    let blobs = receiptContainerClient.listBlobsFlat();
-    for await (const blob of blobs) {
-        if (blob.name === blobName) {
-            return true;
-        }
-    }
-    return false;
+    const blobClient = receiptContainerClient.getBlobClient(blobName);
+    return await blobClient.exists();
 }
 
 module.exports = {
