73ee2d4c70dc84f93c8a03853812d3c8e3d77732
giomella <giomella@emeal.nttdata.com>
2025-12-18 15:15:54 +0100

    [PAGOPA-3453] added unit tests

--- src/test/java/it/gov/pagopa/receipt/pdf/generator/service/helpdesk/impl/HelpdeskServiceImplTest.java
+++ src/test/java/it/gov/pagopa/receipt/pdf/generator/service/helpdesk/impl/HelpdeskServiceImplTest.java
@@ -1,5 +1,7 @@
 package it.gov.pagopa.receipt.pdf.generator.service.helpdesk.impl;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import it.gov.pagopa.receipt.pdf.generator.entity.cart.CartForReceipt;
 import it.gov.pagopa.receipt.pdf.generator.entity.event.BizEvent;
 import it.gov.pagopa.receipt.pdf.generator.entity.event.InfoTransaction;
 import it.gov.pagopa.receipt.pdf.generator.entity.event.User;
@@ -14,12 +16,17 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 import static it.gov.pagopa.receipt.pdf.generator.utils.ObjectMapperUtilsTest.getBizEventFromFile;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
@@ -28,6 +35,12 @@ import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class HelpdeskServiceImplTest {
+
+    private static final String TOKENIZED_DEBTOR_FISCAL_CODE = "tokenizedDebtorFiscalCode";
+    private static final String TOKENIZED_DEBTOR_2_FISCAL_CODE = "tokenizedDebtorFiscalCode2";
+    private static final String TOKENIZED_PAYER_FISCAL_CODE = "tokenizedPayerFiscalCode";
+    private static final String BIZ_EVENT_ID = "biz-event-id";
+    private static final String CART_ID = "a valid cart id";
 
     @Mock
     private PDVTokenizerServiceRetryWrapper pdvTokenizerServiceMock;
@@ -149,5 +162,64 @@ class HelpdeskServiceImplTest {
         assertNull(result.getEventData());
 
         verify(pdvTokenizerServiceMock, times(2)).generateTokenForFiscalCodeWithRetry(anyString());
+    }
+
+    @Test
+    @SneakyThrows
+    void buildCartSuccess() {
+        int totalNotice = 2;
+        List<BizEvent> bizEventList = buildBizEvents(totalNotice);
+        doReturn(TOKENIZED_DEBTOR_FISCAL_CODE, TOKENIZED_DEBTOR_2_FISCAL_CODE, TOKENIZED_PAYER_FISCAL_CODE)
+                .when(pdvTokenizerServiceMock).generateTokenForFiscalCodeWithRetry(anyString());
+
+        CartForReceipt result = assertDoesNotThrow(() -> sut.buildCart(bizEventList));
+
+        assertNotNull(result);
+        assertEquals(CART_ID, result.getEventId());
+        assertNull(result.getStatus());
+        assertNotNull(result.getPayload());
+        assertEquals(TOKENIZED_PAYER_FISCAL_CODE, result.getPayload().getPayerFiscalCode());
+        assertEquals(totalNotice, result.getPayload().getTotalNotice());
+        assertNotNull(result.getPayload().getCart());
+        assertEquals(totalNotice, result.getPayload().getCart().size());
+    }
+
+    @Test
+    @SneakyThrows
+    void buildCartFailPDVException() {
+        int totalNotice = 2;
+        List<BizEvent> bizEventList = buildBizEvents(totalNotice);
+        doThrow(PDVTokenizerException.class)
+                .when(pdvTokenizerServiceMock).generateTokenForFiscalCodeWithRetry(anyString());
+
+        PDVTokenizerException e = assertThrows(PDVTokenizerException.class, () -> sut.buildCart(bizEventList));
+
+        assertNotNull(e);
+    }
+
+    @Test
+    @SneakyThrows
+    void buildCartFailJsonProcessingException() {
+        int totalNotice = 2;
+        List<BizEvent> bizEventList = buildBizEvents(totalNotice);
+        doThrow(JsonProcessingException.class)
+                .when(pdvTokenizerServiceMock).generateTokenForFiscalCodeWithRetry(anyString());
+
+        JsonProcessingException e = assertThrows(JsonProcessingException.class, () -> sut.buildCart(bizEventList));
+
+        assertNotNull(e);
+    }
+
+    private List<BizEvent> buildBizEvents(int totalNotice) throws IOException {
+        List<BizEvent> bizEventList = new ArrayList<>();
+        for (int i = 0; i < totalNotice; i++) {
+            BizEvent bizEvent = getBizEventFromFile("biz-events/biz-event.json");
+            String bizEvenId = BIZ_EVENT_ID + i;
+            bizEvent.setId(bizEvenId);
+            bizEvent.getTransactionDetails().getTransaction().setTransactionId(CART_ID);
+            bizEvent.getPaymentInfo().setTotalNotice(String.valueOf(totalNotice));
+            bizEventList.add(bizEvent);
+        }
+        return bizEventList;
     }
 }
